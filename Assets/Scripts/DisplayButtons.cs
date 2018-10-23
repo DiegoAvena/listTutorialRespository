e240030beb1a0025675cebeb977d45744bf83863
Diego Avena <diegoavena@Diegos-MacBook-Air.local>
2018-10-22 23:09:55 -0700

    different button colors

--- Assets/Scripts/DisplayButtons.cs
+++ Assets/Scripts/DisplayButtons.cs
@@ -12,6 +12,8 @@ public class DisplayButtons: MonoBehaviour {
 
     public int numberOfPlayers;
 
+    private List<Color> usedColors = new List<Color>();
+
     private void Start()
     {
 
@@ -20,23 +22,75 @@ public class DisplayButtons: MonoBehaviour {
         for (int i = 0; i < numberOfPlayers; i++) {
 
             playerTags.Add("player" + i);
+
             GameObject newButton = Instantiate(teleportButtonPrefab);
-            //Button newButton = teleportButtonPrefab;
-
-            /*newButton.GetComponent<Button>().onClick.AddListener(delegate {
-
-                SetButtonClickedText("player" + i.ToString());
-
-            });*/
 
             newButton.name = "teleportToPlayer" + i.ToString() + "button";
             newButton.SendMessage("AddOnClickMethod");
             newButton.transform.SetParent(myPanel);
             newButton.GetComponentInChildren<Text>().text = "player"+i.ToString();
 
+            ColorBlock colorBlock = newButton.GetComponent<Button>().colors;
+            Color newColor = SetIndividualPlayerColors();
+            newColor[3] = 1;
+            colorBlock.normalColor = newColor;
+            newButton.GetComponent<Button>().colors = colorBlock;
+            Debug.Log(newButton.GetComponent<Button>().colors.normalColor);
+
+
         }
 
         //Destroy(GameObject.)
+
+    }
+
+    private Color SetIndividualPlayerColors() {
+
+        Color potentialPlayerColor;
+        bool colorMadeIsValid = false;
+
+        while (true) {
+
+            potentialPlayerColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+            if (usedColors.Count == 0) {
+
+                usedColors.Add(potentialPlayerColor);
+                return potentialPlayerColor;
+
+            }
+
+            for (int i = 0; i < usedColors.Count; i++)
+            {
+
+                for (int b = 0; b < 3; b++) {
+
+                    Debug.Log((int)usedColors[i][b] + " = " + (int)potentialPlayerColor[b]);
+                    if (usedColors[i][b].Equals(potentialPlayerColor[b]) == true)
+                    {
+
+                        colorMadeIsValid = false;
+
+                    }
+                    else
+                    {
+
+                        colorMadeIsValid = true;
+
+                    }
+
+                }
+
+            }
+
+            if (colorMadeIsValid == true) {
+
+                usedColors.Add(potentialPlayerColor);
+                return potentialPlayerColor;
+
+            }
+
+        }
 
     }
 
